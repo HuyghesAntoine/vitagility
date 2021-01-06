ab8846f6a07d3a8b31fc23032b096f56e696614e
Deuzwood <nicolaswallet@hotmail.fr>
2021-01-06 11:29:31 +0100

    Inital

---
+++ src/app.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var path = require('path');
+var port = process.env.PORT || 3000;
+var routes = require('./routes/router');
+
+app.set('port', port);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.disable('x-powered-by');
+
+app.use('/', routes);
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('layout', {
+        path: 'error',
+        message: err,
+        error: err.status,
+    });
+});
+
+http.listen(port, () => {
+    console.log('listening on *:3000');
+});
